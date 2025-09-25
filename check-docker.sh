e8431e5787826351242c6b1bb8edd301b5c589ff
Manish Singh Negi <101799642+manishnegi120491@users.noreply.github.com>
2025-09-25 15:41:17 +0530

    Remove check-docker.sh and clean detection image - production cleanup

--- check-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Checking for Docker files..."
-find . -name "Dockerfile*" -type f
-find . -name "docker-compose*" -type f
-find . -name ".dockerignore" -type f
-echo "Docker file check complete."
